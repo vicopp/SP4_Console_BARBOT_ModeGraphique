86f1b916a7d6987a93a136f521232eb00c7f524f
victor <victor@macbook-2>
2022-12-29 23:25:23 +0100

    commit avancement

---
+++ src/sp4_console_barbot/CelluleGraphique.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_barbot;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author victor
+ */
+public class CelluleGraphique extends JButton {
+    
+    CelluleDeGrille celluleAssociee;
+    
+    
+    
+    
+    public CelluleGraphique  (CelluleDeGrille uneCellule) {
+        celluleAssociee = uneCellule;
+        
+    }
+    
+    
+    
+    
+}
